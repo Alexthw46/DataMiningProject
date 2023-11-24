d0c7008bf3a59dcaec389fdbecbdb95cc6004f4f
Alexthw46 <77152778+Alexthw46@users.noreply.github.com>
2023-11-24 11:19:53 +0100

    revise incident cat, code cleanup

--- TASK_1/feature_extractors.py
+++ TASK_1/feature_extractors.py
@@ -4,22 +4,15 @@ import pandas as pd
 # calculates the ratio of participants in each incident to the total number of participants in all incidents for that
 # specific month and year.
 def ratio_par_to_total(df):
-    total_participants = pd.DataFrame()
-    years = [2014, 2015, 2016, 2017, 2018]
-    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    for x in years:
-        incidents = df[df['year'] == x]
-        for y in months:
-            month_incidents = incidents[incidents['month'] == y]
-            temp = month_incidents.groupby('state', as_index=False).sum()
-            total_involved = temp[['state', 'n_participants']]
-            total_involved['year'] = x
-            total_involved['month'] = y
-            total_participants = pd.concat([total_participants, total_involved])
-    total_participants = total_participants.rename(columns={'n_participants': 'tot_participants'})
-    df = pd.merge(df, total_participants, how='inner')
+    total_participants = (
+        df.groupby(['year', 'month', 'state'], as_index=False)
+        .agg(tot_participants=('n_participants', 'sum'))
+    )
+
+    df = df.merge(total_participants, on=['year', 'month', 'state'], how='left')
     df['par_to_tot'] = df['n_participants'] / df['tot_participants']
-    df = df.drop(columns=['tot_participants'])
+    df.drop(columns=['tot_participants'], inplace=True)
+
     return df
 
 
@@ -135,80 +128,67 @@ def teen_to_f(df):
 
 def incidents_to_win(df, df3):
     incidents_win = pd.DataFrame()
-    years = [2014, 2015, 2016, 2017, 2018]
-    for x in years:
+
+    for x in df['year'].unique():  # Iterating through unique years in the DataFrame
         incidents = df[df['year'] == x]
         state_incidents = incidents['state'].value_counts().rename_axis('state').reset_index(name='counts')
         state_incidents['year'] = x
-        year_results = df3
-        if x % 2 == 0:
-            year_results = year_results[year_results['year'] == x - 2]
-            year_results['year'] = x
-        else:
-            year_results = year_results[year_results['year'] == x - 1]
-            year_results['year'] = x
+
+        year_results = df3[df3['year'].isin([x - 2, x - 1])]
+        year_results.loc[year_results['year'] != x - (x % 2), 'year'] = x
         year_results['win_percentage'] = (year_results['candidatevotes'] / year_results['totalvotes']) * 100
-        year_results = year_results.drop(columns=['congressional_district', 'party', 'candidatevotes', 'totalvotes'])
+        year_results.drop(columns=['congressional_district', 'party', 'candidatevotes', 'totalvotes'], inplace=True)
         year_results = year_results.groupby('state', as_index=False).mean()
-        temp = pd.merge(state_incidents, year_results)
+
+        temp = pd.merge(state_incidents, year_results, on='state')
         incidents_win = pd.concat([incidents_win, temp])
+
     incidents_win['incidents_to_win'] = incidents_win['counts'] / incidents_win['win_percentage']
-    incidents_win = incidents_win.drop(columns=['counts', 'win_percentage'])
-    df = pd.merge(df, incidents_win, how='outer')
+    incidents_win.drop(columns=['counts', 'win_percentage'], inplace=True)
+
+    df = df.merge(incidents_win, how='outer')
     return df
 
 
 def involved_to_voters(df, df3):
     involved_voters = pd.DataFrame()
-    years = [2014, 2015, 2016, 2017, 2018]
-    for x in years:
+
+    for x in df['year'].unique():  # Iterating through unique years in the DataFrame
         incidents = df[df['year'] == x]
-        temp = incidents.groupby('state', as_index=False).sum()
-        temp['n_involved'] = temp['n_killed'] + temp['n_injured']
-        total_involved = temp[['state', 'n_involved']]
-        total_involved['year'] = x
-        year_results = df3
-        if x % 2 == 0:
-            year_results = year_results[year_results['year'] == x - 2]
-            year_results['year'] = x
-        else:
-            year_results = year_results[year_results['year'] == x - 1]
-            year_results['year'] = x
-        year_results = year_results.groupby('state', as_index=False).sum()
-        year_results = year_results.drop(columns=['congressional_district', 'party', 'year', 'totalvotes'])
-        temp = pd.merge(total_involved, year_results)
+        temp = incidents.groupby('state', as_index=False).agg(n_involved=('n_killed', 'sum', 'n_injured', 'sum'))
+
+        year_results = df3[df3['year'].isin([x - 2, x - 1])].groupby('state', as_index=False).sum()
+        year_results.drop(columns=['congressional_district', 'party', 'year', 'totalvotes'], inplace=True)
+
+        temp = temp.merge(year_results, on='state')
+        temp['involved_to_voters'] = (temp['n_involved'] / temp['candidatevotes']) * 100000
+        temp.drop(columns=['n_involved', 'candidatevotes'], inplace=True)
+
         involved_voters = pd.concat([involved_voters, temp])
-    involved_voters['involved_to_voters'] = (involved_voters['n_involved'] / involved_voters['candidatevotes']) * 100000
-    involved_voters = involved_voters.drop(columns=['n_involved', 'candidatevotes'])
-    df = pd.merge(df, involved_voters, how='outer')
+
+    df = df.merge(involved_voters, how='outer')
+
     return df
 
 
 def kill_to_gun(df, df5):
     kills_to_gun = pd.DataFrame()
-    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    years = [2014, 2015, 2016]
-    for x in years:
-        incidents = df[df['year'] == x]
-        for y in months:
-            temp = incidents[incidents['month'] == y]
-            temp = temp.groupby('state', as_index=False).sum()
+
+    for x in df['year'].unique():  # Iterating through unique years in the DataFrame
+        for y in df['month'].unique():  # Iterating through unique months in the DataFrame
+            incidents = df[(df['year'] == x) & (df['month'] == y)]
+            temp = incidents.groupby('state', as_index=False).agg(n_killed=('n_killed', 'sum'))
+
             state_kills = temp[['state', 'n_killed']]
             state_kills['year'] = x
             state_kills['month'] = y
+
             gun_ownership = df5[df5['year'] == x]
             temp = pd.merge(state_kills, gun_ownership, how='inner')
             kills_to_gun = pd.concat([kills_to_gun, temp])
+
     kills_to_gun['kill_to_gun'] = kills_to_gun['n_killed'] / (kills_to_gun['HFR'] * 100)
-    kills_to_gun = kills_to_gun.drop(columns=['n_killed', 'HFR', 'universl', 'permit'])
-    df = pd.merge(df, kills_to_gun, how='outer')
-    return df
+    kills_to_gun.drop(columns=['n_killed', 'HFR', 'universl', 'permit'], inplace=True)
 
-
-def poverty_to_gun(df, df2, df5):
-    temp = df5.rename(columns={'Year': 'year', 'STATE': 'state'})
-    merge = pd.merge(df2, temp, how='inner')
-    merge['poverty_to gun'] = merge['povertyPercentage'] / (merge['HFR'] * 100)
-    merge = merge.drop(columns=['povertyPercentage', 'HFR', 'universl', 'permit'])
-    df = pd.merge(df, merge, how='outer')
+    df = df.merge(kills_to_gun, how='outer')
     return df
