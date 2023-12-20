fb3676df23dfc3a7cfdb6a0b5c164b3ccf8edca6
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2023-12-20 13:16:33 +0100

    svm, nn and knn fixed

--- TASK_3/classifiers.py
+++ TASK_3/classifiers.py
@@ -1,14 +1,22 @@
+import os
+import warnings
+
 import keras as k
 from keras import Sequential
+from keras.src.utils.version_utils import callbacks
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.model_selection import GridSearchCV
 
 
 # Function that finds the best hyperparameter search through CV on the given classifier.
 # Retrains on the whole set at the end.
-def grid_search(classifier, X, y, parameters, folds=None, print_res=True):
+def grid_search(classifier, X, y, parameters, folds=5, print_res=True):
     cvs = GridSearchCV(classifier, parameters, refit=True, cv=folds, scoring='accuracy',
-                       n_jobs=10, verbose=10)
-    cvs.fit(X, y)
+                       n_jobs=max(os.cpu_count() - 2, 1), verbose=4)
+    # Suppress ConvergenceWarning
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=ConvergenceWarning)
+        cvs.fit(X, y)
     if print_res:
         print("Best parameters set found:")
         print(cvs.best_params_)
@@ -24,9 +32,18 @@ def keras_mlp(train_X, train_y, val_X, val_y, optimizer, activation, dropout, ep
             k.Input(shape=13),
             k.layers.Dense(units=50, activation=activation),
             k.layers.Dense(units=25, activation=activation),
+            k.layers.Dense(units=25, activation=activation),
             k.layers.Dropout(dropout),
             k.layers.Dense(1, "sigmoid")
         ]
     )
+    callback = [
+        callbacks.ReduceLROnPlateau(monitor='val_accuracy', mode='max', patience=7, cooldown=2, verbose=1, factor=0.25,
+                                    min_lr=1e-7,
+                                    min_delta=1e-5),
+        callbacks.EarlyStopping(monitor='val_accuracy', mode='max', patience=10, verbose=1, min_delta=1e-5,
+                                restore_best_weights=True)
+    ]
     neural_net.compile(optimizer=optimizer, loss=k.losses.BinaryCrossentropy(), metrics=['accuracy'])
-    return neural_net.fit(train_X, train_y, epochs=epochs, verbose=2, validation_data=(val_X, val_y))
+    return neural_net.fit(train_X, train_y, batch_size=320, epochs=epochs, verbose=2, validation_data=(val_X, val_y),
+                          callbacks=callback, use_multiprocessing=True, workers=10), neural_net
